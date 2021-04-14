d4b900e6dcc51c499856e72d5f6fe06a00c31021
kaido5656 <78507488+kaido5656@users.noreply.github.com>
2021-04-14 13:33:10 -0500

    queries

--- schema.sql
+++ schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE departments(
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
@@ -35,8 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
@@ -50,7 +51,7 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
